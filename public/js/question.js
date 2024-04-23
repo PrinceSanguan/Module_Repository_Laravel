b6bb6cfd27f1cd068d564b2a29ce466f82086b90
Prince Sanguan <princesanguan44@gmail.com>
2024-04-23 21:25:09 +0800

    version 2 update

---
+++ public/js/question.js
@@ -0,0 +1,58 @@
+let questions = [
+    {
+        numb: 1,
+        question: "Ano ang area ng parihaba na nasa Larawan?",
+        answer: "A. 60cm.sq",
+        image: "images/question1.png",
+        options: ["A. 60cm.sq", "B. 45cm.sq", "C. 65cm.sq", "D. 95cm.sq"],
+    },
+    {
+        numb: 2,
+        question:
+            "Ano ang area ng parihaba na nasa Larawan? Gamitin ang formula na SxS.",
+        answer: "D. 64cm.sq",
+        image: "images/question2.png",
+        options: ["A. 74cm.sq", "B. 45cm.sq", "C. 97cm.sq", "D. 64cm.sq"],
+    },
+    {
+        numb: 3,
+        question:
+            "Ano ang area ng parihaba na nasa Larawan? Gamitin ang formula na SxS.",
+        answer: "C. 40cm.sq",
+        image: "images/question3.png",
+        options: ["A. 50cm.sq", "B. 51cm.sq", "C. 40cm.sq", "D. 47cm.sq"],
+    },
+    /*     {
+        numb: 4,
+        question: "What does SQL stand for?",
+        answer: "D. Structured Query Language",
+        options: [
+            "A. Strength Query Language",
+            "B. Stylesheet Query Language",
+            "C. Science Question Language",
+            "D. Structured Query Language",
+        ],
+    },
+    {
+        numb: 5,
+        question: "What does XML stand for?",
+        answer: "D. Extensible Markup Language",
+        options: [
+            "A. Excellent Multiple Language",
+            "B. Explore Multiple Language",
+            "C. Extra Markup Language",
+            "D. Extensible Markup Language",
+        ],
+    },
+    {
+        numb: 6,
+        question: "Ilan ang Paa ng Manok?",
+        answer: "A. Isa Lang",
+        options: [
+            "A. Isa Lang",
+            "B. Dalawa Lang",
+            "C. Tatlo lang",
+            "D. Wala sa Nabanggit",
+        ],
+    }, */
+];
